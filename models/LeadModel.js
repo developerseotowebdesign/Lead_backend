2f2b4e2408ffe36b9c1e56b8ed03106e630751e8
arvindseotowebdesign <arvind.seotowebdesign@gmail.com>
2024-07-31 14:32:05 +0530

    first commit

---
+++ models/LeadModel.js
@@ -0,0 +1,64 @@
+import mongoose from "mongoose";
+
+const LeadSchema = mongoose.Schema(
+  {
+    PickupLocation: {
+      type: String,
+    },
+    DropLocation: {
+      type: String,
+    },
+    LeadId: {
+      type: String,
+    },
+    startDate: {
+      type: Date,
+    },
+    endDate: {
+      type: Date,
+    },
+
+    EmployeeId: {
+      // Changed field name to plural and set type as an array of ObjectIds
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    VendorId: {
+      // Changed field name to plural and set type as an array of ObjectIds
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    BuyId: [
+      {
+        // Changed field name to plural and set type as an array of ObjectIds
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    count: {
+      type: Number,
+      default: 0,
+    },
+    name: {
+      type: String,
+    },
+    phone: {
+      type: Number,
+    },
+    email: {
+      type: String,
+    },
+    type: {
+      type: Number,
+    },
+    status: {
+      type: Number,
+      default: 0,
+    },
+  },
+  { timestamps: true }
+);
+
+const LeadModel = mongoose.model("Lead", LeadSchema);
+
+export default LeadModel;
